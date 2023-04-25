832d948a5d0ca14988878053c875850a8ba4408d
Taha Elsayed <tahazayed@gmail.com>
2023-04-25 11:37:46 -0500

    Add Videos API

---
+++ app/src/main/java/com/android/finalproject/data/model/VideosList.kt
@@ -0,0 +1,12 @@
+package com.android.finalproject.data.model
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import kotlinx.parcelize.Parcelize
+
+@Keep
+@Parcelize
+data class VideosList(
+    val id: Int,
+    val results: List<Video>
+):Parcelable
